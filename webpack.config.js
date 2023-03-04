0dede971b6a5ff4aa18591ce9d453716dc398957
JeisonPC <jeida16@yahoo.es>
2023-03-03 19:27:55 -0500

    Agregando paquetes de babel pára el proyecto:Class 7

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,20 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename:'main.js',
+        filename: 'main.js',
     },
     resolve: {
         extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
     }
 }
