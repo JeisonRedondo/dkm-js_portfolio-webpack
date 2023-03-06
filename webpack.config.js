6d8830ddc3891a331588d155837d60712f1dd3cd
JeisonPC <jeida16@yahoo.es>
2023-03-05 19:43:57 -0500

    generando la configuraicon de la clase 12

--- webpack.config.js
+++ webpack.config.js
@@ -33,14 +33,14 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'assets/images/[name][ext][query]',
+                    filename: 'assets/images/[hash][ext][query]',
                 },
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'assets/fonts/[name][ext]',
+                    filename: 'assets/fonts/[hash][ext]',
                 },
             },
         ]
