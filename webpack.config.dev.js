0ee8ec36212622a01ad3dce5ca97cd128d4d7e6f
JeisonPC <jeida16@yahoo.es>
2023-03-08 15:24:18 -0500

    Agregando cambios de la clase 20

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,6 @@ module.exports = {
         clean:true,
     },
     mode: 'development',
-    watch: true ,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -65,4 +64,16 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+        static:
+        {
+            directory: path.join(__dirname, "dist"),
+            watch: true,
+        },
+        watchFiles: path.join(__dirname, "./**"),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        open: true,
+    },
 }
